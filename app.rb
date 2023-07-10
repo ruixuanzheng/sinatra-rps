c486002dda7f4981a11877c5660a5b749e367edf
ruixuanzheng <96707192+ruixuanzheng@users.noreply.github.com>
2023-07-10 07:33:55 +0000

    1.0

--- app.rb
+++ app.rb
@@ -1,14 +1,62 @@
 require 'sinatra'
 
-get('/') do
-  "
-  <ul>
-    <li><a href=\"Play\">Play Rock</a></li>
-    <li><a href=\"Play\">Play Paper</a></li>
-    <li><a href=\"Play\">Play Scissors</a></li>
-  </ul>
-  <h1>Welcome to Rock-Paper-Scissors!</h1>
-  <p>From<a href=\"https://en.wikipedia.org/wiki/Rock_paper_scissors\">Wikipedia</a></p>
+get("/") do
+  erb(:elephant)
+end
 
-  "
+get("/rock") do
+  plays = ["rock", "paper", "scissors"]
+  they_play = plays.sample
+  if they_play == "rock"
+    result = "We tied!"
+  end
+  if they_play == "paper"
+    result = "We lost!"
+  end
+  if they_play == "scissors"
+    result = "We won!"
+  end
+  @we_played = "We played rock! "
+  @they_played = "They played #{they_play}! "
+  @outcome = "#{result}"
+  erb(:rock_paper_scissors)
+  
+end
+
+get("/paper") do
+  plays = ["rock", "paper", "scissors"]
+  they_play = plays.sample
+  if they_play == "paper"
+    result = "We tied!"
+  end
+  if they_play == "scissors"
+    result = "We lost!"
+  end
+  if they_play == "rock"
+    result = "We won!"
+  end
+  @we_played = "We played paper! "
+  @they_played = "They played #{they_play}! "
+  @outcome = "#{result}"
+  erb(:rock_paper_scissors)
+  
+end
+
+get("/scissors") do
+  plays = ["rock", "paper", "scissors"]
+  they_play = plays.sample
+  if they_play == "scissors"
+    result = "We tied!"
+  end
+  if they_play == "rock"
+    result = "We lost!"
+  end
+  if they_play == "paper"
+    result = "We won!"
+  end
+  @we_played = "We played scissors! "
+  @they_played = "They played #{they_play}! "
+  @outcome = "#{result}"
+  erb(:rock_paper_scissors)
+  
 end
